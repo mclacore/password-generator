9c57adf1a726cab091eaba4b329141c73754e998
mclacore <mclacore@gmail.com>
2024-07-17 09:56:02 -0700

    cleaning up window size

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import *
 root = Tk()
 
 root.title("Password Generator")
-root.geometry("500x500")
+root.geometry("200x200")
 
 menu = Menu(root)
 item = Menu(menu)
@@ -12,18 +12,7 @@ menu.add_cascade(label="File", menu=item)
 root.config(menu=menu)
 
 title_label = Label(root, text="Generate a new password")
-title_label.grid()
-
-
-length = Scale(root, from_=8, to=128, orient=HORIZONTAL, label="Length of password")
-numbers = Checkbutton(root, text="Numbers",
-                    variable=IntVar(),
-                    onvalue=1,
-                    offvalue=0)
-symbols = Checkbutton(root, text="Symbols",
-                      variable=IntVar(),
-                      onvalue=1,
-                      offvalue=0)
+title_label.grid(column=1, row=1)
 
 
 def clicked():
@@ -31,11 +20,19 @@ def clicked():
     title_label.configure(text=result)
 
 
-length.grid(column=3, row=3)
-numbers.grid(column=3, row=4)
-symbols.grid(column=3, row=5)
+length = Scale(root, from_=8, to=128, orient=HORIZONTAL, label="Length of password")
+
+numbers = Checkbutton(root, text="Numbers", variable=IntVar(), onvalue=1, offvalue=0)
+
+symbols = Checkbutton(root, text="Symbols", variable=IntVar(), onvalue=1, offvalue=0)
+
 button = Button(root, text="Generate Password", fg="blue", command=clicked)
-button.grid(column=2, row=0)
+
+
+length.grid(column=1, row=3)
+numbers.grid(column=1, row=4)
+symbols.grid(column=1, row=5)
+button.grid(column=1, row=6)
 
 root.mainloop()
 
